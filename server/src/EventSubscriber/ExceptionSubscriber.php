dc5e606b41cc40a56c963f10d8ba4b776a2eba89
MH-DevApp <mehdih.devapp@gmail.com>
2024-01-07 14:52:16 +0000

    feat: [SERVER] add subscriber for exceptions responses in JSON format type

---
+++ server/src/EventSubscriber/ExceptionSubscriber.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\EventSubscriber;
+
+use Psr\Log\LoggerInterface;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpKernel\Event\ExceptionEvent;
+use Symfony\Component\HttpKernel\Exception\HttpException;
+use Symfony\Component\HttpKernel\KernelEvents;
+
+class ExceptionSubscriber implements EventSubscriberInterface
+{
+    public function __construct(private readonly LoggerInterface $logger)
+    {
+    }
+
+    public function onKernelException(ExceptionEvent $event): void
+    {
+        $exception = $event->getThrowable();
+        $data = [
+            'status' => 500,
+            'message' => $exception->getMessage()
+        ];
+
+        $this->logger->info("Exception Intercepted : ".$exception->getMessage());
+
+        if ($exception instanceof HttpException) {
+            $data['status'] = $exception->getStatusCode();
+        }
+
+        $event->setResponse(new JsonResponse($data, $data['status']));
+    }
+
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            KernelEvents::EXCEPTION => 'onKernelException',
+        ];
+    }
+}
