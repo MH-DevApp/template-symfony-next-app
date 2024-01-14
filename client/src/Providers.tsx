30a011ad141925c4c6c23b8c82c4213a17712d71
mehdi <mehdih.devapp@gmail.com>
2024-01-14 12:18:58 +0100

    feat: [CLIENT] Add Provider with QueryClient for use ReactQuery

---
+++ client/src/Providers.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
+import {PropsWithChildren} from "react";
+
+export const Providers = ({children}: PropsWithChildren) => {
+    const queryClient = new QueryClient();
+
+    return (
+        <QueryClientProvider client={queryClient}>
+            {children}
+        </QueryClientProvider>
+    )
+}
