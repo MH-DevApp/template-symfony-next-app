6c8d6720f1b0ed826695064f07ae29f37c34f159
mehdi <mehdih.devapp@gmail.com>
2024-01-15 23:48:25 +0100

    fix: [CLIENT] refactoring header actions

--- client/src/components/layout/ActionsHeader.tsx
+++ client/src/components/layout/ActionsHeader.tsx
@@ -3,24 +3,32 @@
 import {useSession} from "@/symfauth/session/SessionContext";
 import Link from "next/link";
 
-const ActionsHeader = () => {
-    const {currentUser} = useSession();
-
-    if (!currentUser) {
-        return (
-            <div className="flex items-end gap-3">
-                <Link href={"/auth/signin"} className="hover:underline">Sign in</Link>
-                <span className="text-gray-500">|</span>
-                <Link href={"/auth/signup"} className="hover:underline">Sign up</Link>
-            </div>
-        )
-    }
-
+const AuthenticatedHeader = () => {
     return (
         <div className="flex items-end gap-3">
             <Link href={"/auth/signout"} className="hover:underline">Sign out</Link>
         </div>
     )
+}
+
+const UnauthenticatedHeader = () => {
+    return (
+        <div className="flex items-end gap-3">
+            <Link href={"/auth/signin"} className="hover:underline">Sign in</Link>
+            <span className="text-gray-500">|</span>
+            <Link href={"/auth/signup"} className="hover:underline">Sign up</Link>
+        </div>
+    )
+}
+
+const ActionsHeader = () => {
+    const {currentUser} = useSession();
+
+    if (currentUser !== null) {
+        return <AuthenticatedHeader />
+    }
+
+    return <UnauthenticatedHeader />
 }
 
 export default ActionsHeader;
