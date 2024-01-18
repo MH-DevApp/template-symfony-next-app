d689b9312429474a5dc8aeab83e72b924a3cafab
mehdi <mehdih.devapp@gmail.com>
2024-01-18 23:09:01 +0100

    feat: [CLIENT] Adding fetch next API for server side outside API or Server Action

--- client/src/utils/fetchUtil.ts
+++ client/src/utils/fetchUtil.ts
@@ -46,6 +46,43 @@ export const fetchServerApi = async (url: string, init?: RequestInit) => {
     return responseJson;
 }
 
+export const fetchNextApi = async (url: string, init?: RequestInit) => {
+    if (!process.env.API_URL) {
+        throw new Error("Must be define NEXT_API_URL in .env file");
+    }
+    const tokenAuthName = process.env.SERVER_API_TOKEN_AUTH_NAME ?? DEFAULT_SERVER_API_TOKEN_AUTH_NAME;
+    const tokenApi = cookies().get(tokenAuthName);
+
+    let headers: HeadersInit = {
+        ...defaultHeaders,
+        ...init?.headers ?? {},
+    };
+
+    if (tokenApi) {
+        headers = {
+            ...headers,
+            "cookie": `${tokenAuthName}=${tokenApi.value}`,
+        }
+    }
+
+    const response = await fetch(`${process.env.API_URL}${url}`, {
+        method: init?.method ?? "GET",
+        body: init?.body,
+        headers: headers,
+        cache: init?.cache ?? "default"
+    });
+
+    const responseJson = await response.json();
+
+    if (!response.ok) {
+        if (response.status === 500) {
+            throw new Error("An error occurred, please try again later.");
+        }
+    }
+
+    return responseJson;
+}
+
 export const responseServerApiSchema = ({ fieldsError, dataSchema }: {
     fieldsError: readonly [string, ...string[]];
     dataSchema?: z.ZodSchema;
