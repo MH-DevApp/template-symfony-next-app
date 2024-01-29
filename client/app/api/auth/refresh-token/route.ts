74a6d8950490b1ff510e2020dd258b7decb9079e
mehdi <mehdih.devapp@gmail.com>
2024-01-29 22:03:08 +0100

    feat: [CLIENT] add refresh token feature

---
+++ client/app/api/auth/refresh-token/route.ts
@@ -0,0 +1,54 @@
+import {cookies} from "next/headers";
+
+import {NextResponse} from "next/server";
+import {TokenType} from "@/symfauth/session/SessionContext";
+import {fetchServerApi} from "@/utils/fetchUtil";
+import {COOKIE_JWT_NAME, COOKIE_JWT_TTL, REFRESH_TOKEN_TICK} from "@/utils/defaultValues";
+
+export async function POST() {
+    try {
+        const token = cookies().get(COOKIE_JWT_NAME)?.value.split(".")[1];
+
+        if (!token) {
+            return Response.json({
+                success: false,
+                message: "No token found"
+            });
+        }
+
+
+        const decodedToken: TokenType = JSON.parse(atob(token));
+
+        if (decodedToken.iat + REFRESH_TOKEN_TICK < Date.now() / 1000) {
+            const response: {success: boolean; message: string; data: {tokenRefreshed?: string;}} = await fetchServerApi("auth/refresh-token", {
+                method: "POST",
+                cache: "no-cache"
+            });
+
+            console.log(response);
+
+            if (response.success && response.data.tokenRefreshed) {
+                const { exp: tokenRefreshedExp }: TokenType = JSON.parse(atob(response.data.tokenRefreshed.split(".")[1]));
+
+                cookies().set(COOKIE_JWT_NAME, response.data.tokenRefreshed, {
+                    expires: tokenRefreshedExp ? tokenRefreshedExp * 1000 : new Date(Date.now() + COOKIE_JWT_TTL),
+                    sameSite: "lax"
+                });
+            }
+
+            return Response.json({
+                success: response.success,
+                message: response.message
+            });
+        }
+
+        return Response.json({
+            success: true,
+            message: "Token has not need to be refreshed"
+        });
+
+
+    } catch (e) {
+        return new NextResponse("Erreur", { status: 500 })
+    }
+}
--- client/src/symfauth/session/SessionContext.ts
+++ client/src/symfauth/session/SessionContext.ts
@@ -1,12 +1,14 @@
 import {UserType} from "@/models/User";
 import {createContext, useContext, useEffect, useState} from "react";
 import {useRouter} from "next/navigation";
-import {COOKIE_JWT_NAME} from "@/utils/defaultValues";
+import {COOKIE_JWT_NAME, REFRESH_TOKEN_TICK} from "@/utils/defaultValues";
+import {useQuery} from "@tanstack/react-query";
 
 export type SessionType = {
     currentUser: UserType|null;
     signIn: (user: UserType) => void;
     signOut: () => void;
+    refreshSession: () => Promise<boolean>;
 }
 
 export type TokenType = {
@@ -25,21 +27,21 @@ export const useSession = (): SessionType => {
         throw new Error('useSession must be used within a SessionProvider');
     }
 
+    useQuery({
+        queryKey: ['session_refresh-token'],
+        queryFn: async () => {
+            await session.refreshSession();
+            return session.currentUser;
+        },
+        retry: false
+    });
+
     return session;
 }
 
 export const useSessionProvider = (user: UserType|null): SessionType => {
     const [currentUser, setCurrentUser] = useState<UserType|null>(user);
     const router = useRouter();
-
-    useEffect(() => {
-        if (
-            !user &&
-            document.cookie.includes(COOKIE_JWT_NAME)
-        ) {
-            signOut().then();
-        }
-    }, [])
 
     const signIn = (user: UserType) => {
         setCurrentUser(() => user);
@@ -56,5 +58,35 @@ export const useSessionProvider = (user: UserType|null): SessionType => {
         }
     }
 
-    return {currentUser, signIn, signOut};
+    const refreshSession = async (): Promise<boolean> => {
+        const token = document.cookie
+            .split(";")
+            .find((c) => c.trim().startsWith(COOKIE_JWT_NAME))
+            ?.split("=")[1]
+            ?.split(".")[1];
+
+        if (!token) {
+            if (currentUser) {
+                setCurrentUser(() => null);
+            }
+            return false;
+        }
+
+        const decodedToken: TokenType = JSON.parse(atob(token));
+
+        if (decodedToken.iat + REFRESH_TOKEN_TICK < Date.now() / 1000) {
+            const response = await fetch("/api/auth/refresh-token", { method: "POST", cache: "no-cache"});
+            const responseJson = await response.json();
+
+            if (!responseJson.success) {
+                await signOut();
+            }
+
+            return responseJson.success;
+        }
+
+        return false;
+    }
+
+    return {currentUser, signIn, signOut, refreshSession};
 }
